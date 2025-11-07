d123bdfbd7c7f709536d5d4736367f7d5d014055
Sherin <sherin.daniel89@yahoo.com>
2025-11-06 20:17:41 -0500

    feat ; created database table

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,34 @@
--- TODO
+DROP TABLE orders_products;
+DROP TABLE order;
+DROP TABLE products;
+DROP TABLE users;
+
+
+
+ -- create tables -- order matters -- 
+ CREATE TABLE users(
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+ );
+
+ CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+ );
+
+ CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price numeric(2) NOT NULL
+ );
+
+ CREATE TABLE orders_products(
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+ );
